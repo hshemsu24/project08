57e2334f2182a941a56695f7d652613219bbd7cd
Hamza Shemsu <hshemsu10@gmail.com>
2023-11-17 10:05:08 -0500

    create vite app

---
+++ footballhub/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
